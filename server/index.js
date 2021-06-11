7634ca91b42b42dda76193f88e9ddb4810ef499b
Jodi Silverman <jodimsilverman@gmail.com>
2021-06-11 17:17:21 -0400

    upload to heroku

---
+++ server/index.js
@@ -0,0 +1,37 @@
+//================================================================================================================
+// SERVER CONFIGURATION
+//================================================================================================================
+const path = require('path');
+const express = require("express");
+
+const PORT = process.env.PORT || 3030;
+
+const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+//================================================================================================================
+// APPLICATION SERVER ROUTES
+//================================================================================================================
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
+})
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server at api!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+//================================================================================================================
+// SERVER LISTENING
+//================================================================================================================
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}. To get started, visit: http://localhost:${PORT}`);
+});
