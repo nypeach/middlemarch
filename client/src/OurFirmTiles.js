eceeb20fe31fe9a96abfaea6ec1ac186c0886a30
Jodi Silverman <jodimsilverman@gmail.com>
2021-07-01 18:21:00 -0400

    Updated all to vmin Fixed Our Team

--- client/src/OurFirmTiles.js
+++ client/src/OurFirmTiles.js
@@ -32,14 +32,14 @@ const OurFirmTiles = () => {
             <img className="column-img-merch" src={merchantBank} alt="Merchant Bank" />
           </div>
           <div className="column column-heading"><h3 className="heading-secondary-dk">Merchant Banking</h3></div>
-          <div className="column column-text">The merchant banking model combines the capital raising and M&amp;A advisory services of an investment bank with the ability to invest like a private equity fund in client companies.&nbsp;&nbsp;<button className="read-more-link" style={{ fontSize: "2rem" }} onClick={isMerchOpenToggle}>Read More ...</button></div>
+          <div className="column column-text">The merchant banking model combines the capital raising and M&amp;A advisory services of an investment bank with the ability to invest like a private equity fund in client companies.&nbsp;&nbsp;<button className="read-more-link" onClick={isMerchOpenToggle}>Read More ...</button></div>
         </div>
         <div className="row" onClick={isPrivOpenToggle}>
           <div className="column column-img-container">
             <img className="column-img-priv" src={privateCap} alt="Private Capital" />
           </div>
           <div className="column column-heading"><h3 className="heading-secondary-dk">Private Capital</h3></div>
-          <div className="column column-text">The principals of Middlemarch Capital Partners, the firm’s investment arm, can either co-invest alongside top-tier sponsors or lead investments.&nbsp;&nbsp;<button className="read-more-link" style={{ fontSize: "2rem" }} onClick={isPrivOpenToggle}>Read More ...</button></div>
+          <div className="column column-text">The principals of Middlemarch Capital Partners, the firm’s investment arm, can either co-invest alongside top-tier sponsors or lead investments.&nbsp;&nbsp;<button className="read-more-link" onClick={isPrivOpenToggle}>Read More ...</button></div>
         </div>
       </div>
 
--- client/src/OurTeam.js
+++ client/src/OurTeam.js
@@ -1,12 +1,103 @@
 import React from 'react';
 import './index.scss';
 import './OurTeam.scss';
+import team1 from './images/team1.jpeg';
+import team2 from './images/team2.jpeg';
+import team3 from './images/team3.jpeg';
+
+
 
 const OurTeam = () => {
   return (
     <section className="section team">
       <h2 className="heading heading-primary heading-primary-wt">Our Team</h2>
+      <div className="team-card-div">
 
+        <div className="tmain">
+          <div className="tcontainer">
+            <div className="timag1"></div>
+            <div className="ttext-container">
+              <span className="tspan"></span>
+              <h1 className="th1">Web Development</h1>
+              <p className="tp">Get the lastest and trending web development project's source code, first see preview if you need then get the code.
+              </p>
+              <a className="ta" href="https://webdevtrick.com" target="_blank">Visit Webdevtrick</a>
+            </div>
+          </div>
+          <div className="tcontainer">
+            <div className="timag2"></div>
+            <div className="ttext-container">
+              <span className="tspan"></span>
+              <h1 className="th1">Web Development</h1>
+              <p className="tp">Get the lastest and trending web development project's source code, first see preview if you need then get the code.
+              </p>
+              <a className="ta" href="https://webdevtrick.com" target="_blank">Visit Webdevtrick</a>
+            </div>
+          </div>
+          <div className="tcontainer">
+            <div className="timag3"></div>
+            <div className="ttext-container">
+              <span className="tspan"></span>
+              <h1 className="th1">Web Development</h1>
+              <p className="tp">Get the lastest and trending web development project's source code, first see preview if you need then get the code.
+              </p>
+              <a className="ta" href="https://webdevtrick.com" target="_blank">Visit Webdevtrick</a>
+            </div>
+          </div>
+          <div className="tcontainer">
+            <div className="timag4"></div>
+            <div className="ttext-container">
+              <span className="tspan"></span>
+              <h1 className="th1">Web Development</h1>
+              <p className="tp">Get the lastest and trending web development project's source code, first see preview if you need then get the code.
+              </p>
+              <a className="ta" href="https://webdevtrick.com" target="_blank">Visit Webdevtrick</a>
+            </div>
+          </div>
+          <div className="tcontainer">
+            <div className="timag2"></div>
+            <div className="ttext-container">
+              <span className="tspan"></span>
+              <h1 className="th1">Web Development</h1>
+              <p className="tp">Get the lastest and trending web development project's source code, first see preview if you need then get the code.
+              </p>
+              <a className="ta" href="https://webdevtrick.com" target="_blank">Visit Webdevtrick</a>
+            </div>
+          </div>
+          <div className="tcontainer">
+            <div className="timag3"></div>
+            <div className="ttext-container">
+              <span className="tspan"></span>
+              <h1 className="th1">Web Development</h1>
+              <p className="tp">Get the lastest and trending web development project's source code, first see preview if you need then get the code.
+              </p>
+              <a className="ta" href="https://webdevtrick.com" target="_blank">Visit Webdevtrick</a>
+            </div>
+          </div>
+
+        </div>
+
+        {/* <div className="team-card-container">
+          <div className="team-card-row">
+            Sasha Grutman
+          </div>
+          <div className="team-card-row">
+            Someone Else
+          </div>
+          <div className="team-card-row">
+            Someone Else
+          </div>
+          <div className="team-card-row">
+            Someone Else
+          </div>
+          <div className="team-card-row">
+            Someone Else
+          </div>
+          <div className="team-card-row">
+            Someone Else
+          </div>
+        </div> */}
+      </div>
     </section>
   );
 }
