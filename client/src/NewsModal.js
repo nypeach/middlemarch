a338bbce3d23ac9c85370bf2f8ca6046c05c5a63
Jodi Silverman <jodimsilverman@gmail.com>
2021-12-27 09:48:27 -0500

    Updated News Modal with Popover

--- client/src/NewsModal.js
+++ client/src/NewsModal.js
@@ -4,7 +4,9 @@ import { newsarticles } from './data/newsarticles.js';
 import articles from './images/news/articles-lt.png';
 import events from './images/news/events-lt.png';
 import news from './images/news/news-lt.png';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import Overlay from 'react-bootstrap/Overlay';
+import Popover from 'react-bootstrap/Popover'
 
 function importAll(r) {
   let pdfs = {};
@@ -18,52 +20,67 @@ const newspdfs = importAll(require.context('./news/news', false, /\.(png|jpe?g|s
 const articlespdfs = importAll(require.context('./news/articles', false, /\.(png|jpe?g|svg|pdf)$/))
 
 
+
 const NewsModal = (props) => {
   const modalImage = props.modalType === 'NEWS' ? news : props.modalType === 'ARTICLES' ? articles : events;
   const [summary, setSummary] = useState('');
+  const [show, setShow] = useState(false);
+  const [target, setTarget] = useState(null);
+  const ref = useRef(null);
 
   return (
     <Modal
       scrollable={false}
       {...props}
-      // size="sm"
       aria-labelledby="NewsModal"
-    // DialogClassName="modal-90w"
     >
       <i className="far fa-times-circle fa-3x news-modal-close" onClick={props.onHide}></i>
       <Modal.Header>
         <Modal.Title>
-             <div className="news-container">
-              <div style={{ alignSelf: "center" }}><img className="news-modal-image" alt={props.modalType} src={modalImage} /></div>
-              <div className="news-modal-header">{props.modalType}</div>
-            <div className="news-summary"><em>{summary}</em></div>
-            </div>
+          <div className="news-container">
+            <div style={{ alignSelf: "center" }}><img className="news-modal-image" alt={props.modalType} src={modalImage} /></div>
+            <div className="news-modal-header">{props.modalType}</div>
+          </div>
         </Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
         <div className="news-modal-li">
           <ul className="no-bullets">
-            <div className="news-modal-text" style={{marginTop: "12rem"}}>
-            {newsarticles.map(item =>
-              item.Type === props.modalType  ?
-                <li key={item.Sort} style={{ marginBottom: "4rem" }}>
-
-                  <div className="news-div" style={{ cursor: "pointer" }} >
-                    <a
-                      href={props.modalType === 'NEWS' ? newspdfs[item.Link] : props.modalType === 'ARTICLES' ? articlespdfs[item.Link] : item.Link}
-                      target="_blank" rel="noreferrer"
-                    >
-                      {/* <span className={props.modalType === 'NEWS' || props.modalType === 'ARTICLES' ? "news-tooltiptext tt-news" : "news-tooltiptext tt-events"}>{item.Summary}</span> */}
-                      <span className="news-tooltiptext tt-modal" style={{width: "25vw"}}>{item.Summary}</span>
-                      {item.Title}
-                    </a>
-                  </div>
-
-                </li>
-                :
-                null
-            )}
+            <div ref={ref} className="news-modal-text" style={{ marginTop: "8rem" }}>
+              {newsarticles.map(item =>
+                item.Type === props.modalType ?
+                  <li
+                    key={item.Sort}
+                    style={{ marginBottom: "4rem" }}
+                    onMouseEnter={(event) => { setSummary(item.Summary); setShow(true); setTarget(event.target) }}
+                    onMouseLeave={(event) => { setSummary(''); setShow(false); setTarget(null) }}
+                  >
+                    <div className="news-div" style={{ cursor: "pointer" }} >
+                      <a
+                        href={props.modalType === 'NEWS' ? newspdfs[item.Link] : props.modalType === 'ARTICLES' ? articlespdfs[item.Link] : item.Link}
+                        target="_blank" rel="noreferrer"
+                      >
+                        {item.Title}
+                      </a>
+                    </div>
+                  </li>
+                  :
+                  null
+              )}
+              <Overlay
+                show={show}
+                target={target}
+                placement="top"
+                container={ref}
+                containerPadding={20}
+              >
+                <Popover id="news-modal-popover">
+                  <Popover.Body className="news-modal-popover-text">
+                    {summary}
+                  </Popover.Body>
+                </Popover>
+              </Overlay>
             </div>
           </ul>
 
