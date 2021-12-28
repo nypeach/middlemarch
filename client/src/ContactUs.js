64b5789ba172272eff69f4e5578fb518049e0e76
Jodi Silverman <jodimsilverman@gmail.com>
2021-12-28 11:19:56 -0500

    Updated Footer with italic

--- client/src/ContactUs.js
+++ client/src/ContactUs.js
@@ -58,6 +58,7 @@ const ContactUs = (props) => {
           <div className="contact-container">
             <Container fluid className="g-0 align-items-stretch">
               <Row>
+
                 <Col lg={4} className="contact-col g-0">
                   <div className="map1">
                     <h3 className="map1-header" >CONNECTICUT OFFICE</h3>
@@ -76,11 +77,13 @@ const ContactUs = (props) => {
                       <br></br>
                       <i className="fas fa-phone" >&nbsp;&nbsp;&nbsp;&nbsp;<em>212-913-9660</em></i>
                     </div>
-                    <iframe title="Connecticut Office" className="gmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3009.11358640147!2d-73.53422348482826!3d41.04464507929712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c29f61d87b8a65%3A0x6237354b63450802!2s700%20Canal%20St%2C%20Stamford%2C%20CT%2006902!5e0!3m2!1sen!2sus!4v1640475781399!5m2!1sen!2sus" width={mapWidth} height={mapHeight} frameBorder="0" style={{ border: 0 }} allowFullScreen="" aria-hidden="false" tabIndex="0" loading="lazy" />
+                    <div className="gmap-div">
+                      <iframe title="Connecticut Office" className="gmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3009.11358640147!2d-73.53422348482826!3d41.04464507929712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c29f61d87b8a65%3A0x6237354b63450802!2s700%20Canal%20St%2C%20Stamford%2C%20CT%2006902!5e0!3m2!1sen!2sus!4v1640475781399!5m2!1sen!2sus" width={mapWidth} height={mapHeight} frameBorder="0" style={{ border: 0 }} allowFullScreen="" aria-hidden="false" tabIndex="0" loading="lazy" />
+                    </div>
                   </div>
                 </Col>
                 <Col lg={4} className="contact-col g-0">
-                  <div className="map2">
+                  <div className="map1">
                     <h3 className="map1-header" >NEW YORK OFFICE</h3>
                     <div className="map1-text1">
                       125 Park Avenue, Suite 1700
@@ -97,13 +100,15 @@ const ContactUs = (props) => {
                       <br></br>
                       <i className="fas fa-phone">&nbsp;&nbsp;&nbsp;&nbsp;<strong><em>212-913-9660</em></strong></i>
                     </div>
-                    <iframe title="New York Office" className="gmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.4681519309192!2d-73.97963938483831!3d40.751726979327614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259012bf7f7c7%3A0x391d930ce98ab3d0!2s125%20Park%20Ave%2C%20New%20York%2C%20NY%2010017!5e0!3m2!1sen!2sus!4v1640479027141!5m2!1sen!2sus" width={mapWidth} height={mapHeight} frameBorder="0" style={{ border: 0 }} allowFullScreen="" aria-hidden="false" tabIndex="0" loading="lazy" />
+                    <div className="gmap-div">
+                      <iframe title="New York Office" className="gmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.4681519309192!2d-73.97963938483831!3d40.751726979327614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259012bf7f7c7%3A0x391d930ce98ab3d0!2s125%20Park%20Ave%2C%20New%20York%2C%20NY%2010017!5e0!3m2!1sen!2sus!4v1640479027141!5m2!1sen!2sus" width={mapWidth} height={mapHeight} frameBorder="0" style={{ border: 0 }} allowFullScreen="" aria-hidden="false" tabIndex="0" loading="lazy" />
+                    </div>
                   </div>
                 </Col>
                 <Col lg={4} className="contact-col g-0 align-self-stretch">
                   <div className="mail-list">
                     <div className="contact-input-box">
-                      <h3 className="map1-header" style={{marginTop: "3rem"}} >JOIN OUR MAILING LIST</h3>
+                      <h3 className="map1-header" style={{ marginTop: "3rem" }} >JOIN OUR MAILING LIST</h3>
 
                       {/* =========== CONFIRMATION PARTNER LOGIN ============ */}
                       {showPartner ?
@@ -176,11 +181,11 @@ const ContactUs = (props) => {
                             value={values.phone}
                             placeholder="Telephone"
                             onChange={handleInputChange}
-                            className = "contact-phone"
+                            className="contact-phone"
                           />
                         </Form.Group>
 
-                        <Button size="md" variant="primary" type="submit" style={{marginBottom: "2rem"}}
+                        <Button size="md" variant="primary" type="submit" style={{ marginBottom: "2rem" }}
                           onClick={(e) => {
                             e.preventDefault();
                             sendEmail(data);
@@ -196,17 +201,17 @@ const ContactUs = (props) => {
 
               </Row>
               <div className="footer-top">&nbsp;</div>
-              <Row className="g-0" style={{margin: "0rem 3rem"}}>
-                  <div className="footer1" style={{ textAlign: "left", padding: "2rem 2.5rem 0rem 2rem" }}>
-                    <div style={{margin: "0rem 0rem 1rem 0rem"}}><em>Both Middlemarch Securities LLC and Middlemarch Capital Partners LLC are separate, wholly-owned subsidiaries of Middlemarch Partners LLC.  Securities Offered Through Middlemarch Securities, LLC, Member FINRA, SIPC.</em>
-                    </div>
-                  <div style={{ display: "inline", textDecoration: "underline" }}><a href="https://brokercheck.finra.org/firm/summary/283508"><em>BrokerCheck</em></a></div>
-                  <div style={{ display: "inline"}}>&nbsp;&nbsp;|&nbsp;&nbsp;</div>
-                  <div style={{ display: "inline", textDecoration: "underline"}}>
-                      <a href="https://files.brokercheck.finra.org/crs_283508.pdf"><em>FINRA Form CRS</em></a></div>
-                  <div style={{ marginTop: "1rem" }}>Material on this website has been prepared as a matter of general information. Middlemarch and its affiliates, employees, and clients may have recently established or disposed of, or may be establishing or disposing of, positions in securities mentioned. (Middlemarch Securities, LLC 2017)</div>
-                    <div style={{ marginTop: "2rem" }}><em>©  2022 Copyright Middlemarch Partners LLC. All Rights Reserved.</em></div>
+              <Row className="g-0" style={{ margin: "0rem 3rem" }}>
+                <div className="footer1" style={{ textAlign: "left", padding: "2rem 2.5rem 0rem 2rem" }}>
+                  <div style={{ margin: "0rem 0rem 1rem 0rem" }}><em>Both Middlemarch Securities LLC and Middlemarch Capital Partners LLC are separate, wholly-owned subsidiaries of Middlemarch Partners LLC.  Securities Offered Through Middlemarch Securities, LLC, Member FINRA, SIPC.</em>
                   </div>
+                  <div style={{ display: "inline", textDecoration: "underline" }}><a href="https://brokercheck.finra.org/firm/summary/283508"><em>BrokerCheck</em></a></div>
+                  <div style={{ display: "inline" }}>&nbsp;&nbsp;|&nbsp;&nbsp;</div>
+                  <div style={{ display: "inline", textDecoration: "underline" }}>
+                    <a href="https://files.brokercheck.finra.org/crs_283508.pdf"><em>FINRA Form CRS</em></a></div>
+                  <div style={{ marginTop: "1rem" }}><em>Material on this website has been prepared as a matter of general information. Middlemarch and its affiliates, employees, and clients may have recently established or disposed of, or may be establishing or disposing of, positions in securities mentioned. (Middlemarch Securities, LLC 2017)</em></div>
+                  <div style={{ marginTop: "2rem" }}><em>©  2022 Copyright Middlemarch Partners LLC. All Rights Reserved.</em></div>
+                </div>
               </Row>
             </Container>
 
