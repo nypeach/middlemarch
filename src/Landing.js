1b7a480e279d08878576612b1191dd9d2d4d4d52
Jodi Silverman <jodimsilverman@gmail.com>
2023-08-12 09:46:08 -0400

    Testing

--- src/Landing.js
+++ src/Landing.js
@@ -13,7 +13,7 @@ const Landing = () => {
           <br></br>
           <img src={mmpartners} alt="logo" className="landing-logo" />
         </h1>
-        <div className="landing-tagline">Financing and Investing in Global Growth Companies</div>
+        <div className="landing-tagline">Financing and Investing in Global Growth Companies.</div>
       </div>
     </header>
   );
